c72b56f771ca5a56bf35894824fb447ee7c9dc22
ChuDatCN <79975646+ChuDatCN@users.noreply.github.com>
2022-08-15 16:20:21 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Website.Models;
+using Umbraco.Extensions;
+using Umbraco9;
+
+namespace UmbracoBackofficeOidc
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup" /> class.
+        /// </summary>
+        /// <param name="webHostEnvironment">The web hosting environment.</param>
+        /// <param name="config">The configuration.</param>
+        /// <remarks>
+        /// Only a few services are possible to be injected here https://github.com/dotnet/aspnetcore/issues/9337
+        /// </remarks>
+        public Startup(IWebHostEnvironment webHostEnvironment, IConfiguration config)
+        {
+            _env = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
+            _config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        /// <summary>
+        /// Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <remarks>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        /// </remarks>
+        public void ConfigureServices(IServiceCollection services)
+        {
+#pragma warning disable IDE0022 // Use expression body for methods
+            
+            var scheme = $"{Constants.Security.BackOfficeExternalAuthenticationTypePrefix}oidc";
+
+            services.AddUmbraco(_env, _config)
+                .AddBackOffice()  //  Cung  cấp components cần tiết của BackOffice
+                .AddBackOfficeExternalLogins(loginsBuilder =>                 // Cung cấp cấu hình login bên ngoài của umbraco
+                    loginsBuilder.AddBackOfficeLogin(authBuilder =>                         // Đưa ra các cách thức Authen để login , vd ở dưới là OpenID
+                            authBuilder.AddOpenIdConnect(scheme, "OpenID Connect", options =>      
+                            {
+                                options.Authority = "https://localhost:5001";  // Khi call phương thức login = OpenID  gọi địa chỉ này
+                                options.ClientId = "umbraco-backoffice";        // khai báo clientID
+                                options.ClientSecret = "secret";                
+                                
+                                options.CallbackPath = "/signin-oidc";          // 
+                                options.ResponseType = "code";
+                                //options.ResponseMode = "query";
+                                options.UsePkce = true;
+                                // get user identity
+                                options.Scope.Add("email");
+                                options.GetClaimsFromUserInfoEndpoint = true;
+                                options.SaveTokens = true;
+                                //options.SignedOutRedirectUri = "https://localhost:5001";
+                                
+
+
+                            }),
+                        providerOptions => 
+                            {
+                                providerOptions.Icon = "fa fa-openid";
+                                //providerOptions.DenyLocalLogin = true;
+                                //providerOptions.AutoRedirectLoginToExternalProvider = true;
+                            }))
+                .AddWebsite()
+                .AddComposers()
+                .Build();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie";
+                options.DefaultChallengeScheme = "oidc";
+            }).AddCookie("cookie");
+
+
+
+
+#pragma warning restore IDE0022 // Use expression body for methods
+        }
+
+        /// <summary>
+        /// Configures the application.
+        /// </summary>
+        /// <param name="app">The application builder.</param>
+        /// <param name="env">The web hosting environment.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseUmbraco()
+                .WithMiddleware(u =>
+                {
+                    u.UseBackOffice();
+                    u.UseWebsite();
+                })
+                .WithEndpoints(u =>
+                {
+                    u.UseInstallerEndpoints();
+                    u.UseBackOfficeEndpoints();
+                    u.UseWebsiteEndpoints();
+                });
+        }
+
+        //public static IUmbracoBuilder AddOpenIdConnectAuthentication(this IUmbracoBuilder builder)
+        //{
+        //    // Register OpenIdConnectBackOfficeExternalLoginProviderOptions here rather than require it in startup
+        //    builder.Services.ConfigureOptions<OpenIdConnectBackOfficeExternalLoginProviderOptions>();
+
+        //    builder.AddBackOfficeExternalLogins(logins =>
+        //    {
+        //        logins.AddBackOfficeLogin(
+        //            backOfficeAuthenticationBuilder =>
+        //            {
+        //                backOfficeAuthenticationBuilder.AddOpenIdConnect(
+        //                    // The scheme must be set with this method to work for the back office
+        //                    backOfficeAuthenticationBuilder.SchemeForBackOffice(OpenIdConnectBackOfficeExternalLoginProviderOptions.SchemeName),
+        //                    options =>
+        //                    {
+        //                // use cookies
+        //                        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        //                // pass configured options along
+        //                        options.Authority = "https://localhost:5001";
+        //                        options.ClientId = "YOURCLIENTID";
+        //                        options.ClientSecret = "YOURCLIENTSECRET";
+        //                // Use the authorization code flow
+        //                        options.ResponseType = OpenIdConnectResponseType.Code;
+        //                        options.AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet;
+        //                // map claims
+        //                        options.TokenValidationParameters.NameClaimType = "name";
+        //                        options.TokenValidationParameters.RoleClaimType = "role";
+
+
+        //                        options.RequireHttpsMetadata = true;
+        //                        options.GetClaimsFromUserInfoEndpoint = true;
+        //                        options.SaveTokens = true;
+        //                // add scopes
+        //                        options.Scope.Add("openid");
+        //                        options.Scope.Add("email");
+        //                        options.Scope.Add("roles");
+        //                        options.UsePkce = true;
+        //                    });
+        //            });
+        //    });
+        //    return builder;
+        //} 
+    }
+}
---
+++ UmbracoCustomOwinStartup.cs
@@ -0,0 +1,85 @@
+﻿//public class UmbracoCustomOwinStartup : UmbracoOwinStartup
+//{
+//    static readonly string Caption = "Keycloak";
+//    static readonly string ClientId = "umbraco";
+//    static readonly string Style = "btn-github";
+//    static readonly string Icon = "fa-key";
+
+//    /// <summary>
+//    /// Configures the <see cref="BackOfficeUserManager"/> for Umbraco
+//    /// </summary>
+//    /// <param name="app"></param>
+//    protected override void ConfigureUmbracoUserManager(IAppBuilder app)
+//    {
+//        // There are several overloads of this method that allow you to customize the BackOfficeUserManager or even custom BackOfficeUserStore.
+//        app.ConfigureUserManagerForUmbracoBackOffice(
+//            Services,
+//            Mapper,
+//            UmbracoSettings.Content,
+//            GlobalSettings,
+//            //The Umbraco membership provider needs to be specified in order to maintain backwards compatibility with the 
+//            // user password formats. The membership provider is not used for authentication, if you require custom logic
+//            // to validate the username/password against an external data source you can create create a custom UserManager
+//            // and override CheckPasswordAsync
+//            global::Umbraco.Core.Security.MembershipProviderExtensions.GetUsersMembershipProvider().AsUmbracoMembershipProvider());
+//    }
+
+//    protected override void ConfigureUmbracoAuthentication(IAppBuilder app)
+//    {
+//        base.ConfigureUmbracoAuthentication(app);
+
+//        app.UseUmbracoBackOfficeTokenAuth(new BackOfficeAuthServerProviderOptions());
+
+//        var identityOptions = new OpenIdConnectAuthenticationOptions
+//        {
+//            Caption = Caption,
+//            Authority = "http://localhost:8080/",
+//            AuthenticationType = "http://localhost:8080/",
+//            ClientId = ClientId,
+//            RedirectUri = "http://umbraco.bifrost.localhost/umbraco",
+//            PostLogoutRedirectUri = "http://umbraco.bifrost.localhost/umbraco",
+//            ResponseType = "code",
+//            Scope = "openid profile roles email",
+//            RequireHttpsMetadata = false,
+//            MetadataAddress = "http://localhost:8080/auth/realms/umbracorealm/.well-known/openid-configuration",
+//            SignInAsAuthenticationType = Constants.Security.BackOfficeExternalAuthenticationType,
+//            Notifications = new OpenIdConnectAuthenticationNotifications
+//            {
+//                SecurityTokenValidated = ClaimsTransformer.GenerateUserIdentityAsync
+//            }
+//        };
+
+//        identityOptions.ForUmbracoBackOffice(Style, Icon);
+//        identityOptions.Caption = Caption;
+//        identityOptions.AuthenticationType = "http://localhost:8080/";
+
+//        var providerOptions = new BackOfficeExternalLoginProviderOptions { AutoLinkOptions = new ExternalSignInAutoLinkOptions(true) };
+//        identityOptions.SetBackOfficeExternalLoginProviderOptions(providerOptions);
+
+//        app.UseOpenIdConnectAuthentication(identityOptions);
+//    }
+//}
+//public class ClaimsTransformer
+//{
+//    public static async Task GenerateUserIdentityAsync(
+//        SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+//    {
+//        // Now this contains ID claims (e.g. GivenName in my case)
+//        var id = notification.AuthenticationTicket.Identity;
+
+//        var identityUser = new ClaimsIdentity(
+//            id.Claims, // copy the claims I have
+//            notification.AuthenticationTicket.Identity.AuthenticationType,
+//            // set the nameType, so Umbraco can use the 'ExternalLogin.Name' for auto-link to work
+//            ClaimTypes.GivenName, // <-- You have to set a correct nameType claim
+//            ClaimTypes.Role);
+
+//        notification.AuthenticationTicket = new AuthenticationTicket(identityUser,
+//               notification.AuthenticationTicket.Properties);
+//    }
+//}
+
+
+
+
+
